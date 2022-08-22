7c44c802e9af34133bf790d8207a25bc17e8c6cc
evelynvsx <102490601+evelynvsx@users.noreply.github.com>
2022-08-22 12:01:35 +1200

    Adding home page information and slider
    Added CSS and javascript for the home page slider but it's not working

--- index.php
+++ index.php
@@ -1,26 +1,15 @@
-<?php
-    /* Connect to the database */
-    $dbcon = mysqli_connect("localhost", "salimev", "hugeknot78", "salimev_spillthetea");
-
-    /* If connection fails, exit nicely */
-    if($dbcon == NULL) {
-        echo "Could not connect to database";
-        exit();
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Spill the Tea</title>
-        <link rel="stylesheet" href="spillthetea.css">
+        <link rel="stylesheet" href="css/spillthetea.css">
     </head>
 
     <body>
         <!-- Side wide navigation bar -->
         <nav>
             <!-- Logo -->
-            <p><a href="index.php"><img id="logo" src="logo.png"</a></p>
+            <p><a href="index.php"><img id="logo" src="images/logo.png"</a></p>
 
             <!-- Navigation links --->
             <a href="index.php">Home</a>
@@ -28,4 +17,44 @@
             <a href="add.php">Add</a>
             <a href="locations.php">Locations</a>
         </nav>
+
+        <br><br>
+
+        <div class="slider_container">
+            
+            <div class="slider_text">
+                <h1>Spill the Tea</h1>
+                <p>Description</p>
+            </div>
+
+            <div class="mySlides fade">
+                <img src="images/slider-img-1" alt="1st image slider" style="width: 80%">
+            </div>
+
+            <div class="mySlides fade">
+                <img src="images/slider-img-2" alt="2nd image slider" style="width: 80%">
+            </div>
+
+            <div class="mySlides fade">
+                <img src="images/slider-img-3" alt="3rd image slider" style="width: 80%">
+            </div>
+
+            <div class="mySlides fade">
+                <img src="images/slider-img-4" alt="4th image slider" style="width: 80%">
+            </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)"><</a>
+            <a class="next" onclick="plusSlides(1)">></a>
+        </div>
+        <br>
+
+    <!-- Dots / circles on bottom of the slider-->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+
     </body>
+</html>
