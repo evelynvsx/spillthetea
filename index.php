1237787ab1e10a147affe8bef37816a052894320
evelynvsx <102490601+evelynvsx@users.noreply.github.com>
2022-09-05 12:24:26 +1200

    added new character set
    so that the special character in the footer dissapears

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="utf-8">
         <title>Spill the Tea</title>
         <link rel="stylesheet" href="css/spillthetea.css">
     </head>
