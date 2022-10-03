43a5ea84034fdd16e8944aca144ffecd9633ee6a
evelynvsx <102490601+evelynvsx@users.noreply.github.com>
2022-10-03 15:08:54 +1300

    adding about us page

--- contact.php
+++ contact.php
@@ -27,6 +27,7 @@
             <a href="index.php">Home</a>
             <a href="menu.php">Menu</a>
             <a href="updatepage.php">Update</a>
+            <a href="aboutus.php">About Us</a>
             <a href="contact.php" class="active">Contact</a>
 
             <!-- Login and logout links -->
@@ -63,7 +64,7 @@
                         <br> <br><!-- break for space between one text box and another -->
 
                         <!-- Submit button -->
-                        <button id="contact-submit-button" type="submit" name="submit">Submit</button>
+                        <button id="contact-submit-button" type="submit" name="submit">Submit</button> <br><br>
                     </div>
 
                     <div class="contact-form-right-column">
@@ -74,6 +75,21 @@
                     </div>
                 </form>
                 <!-- Contact form end -->
+            </div>
+
+            <br> <br> <br> <!-- Separate contact form with store location -->
+
+            <!-- Locations content -->
+            <div class="locations-content">
+                <!-- Locations page information -->
+                <h2>OUR LOCATIONS</h2>
+                <p>500 COURTENAY PLACE, WELLINGTON CBD</p> <br>
+
+                <br> <!-- Break to give space between embedded google map and information -->
+
+                <!-- Embedded google map -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2997.7108274802827!2d174.77902251535988!3d-41.29339757927297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38afda81e644f9%3A0x61a34c646d9ac821!2sCourtenay%20Place%2C%20Te%20Aro%2C%20Wellington%206011!5e0!3m2!1sen!2snz!4v1664698783561!5m2!1sen!2snz"
+                        width="90%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </main>
 
